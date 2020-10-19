28321a470fdc93a4b54c5cffbf0d580acfcc5874
Lily Eichler <lilyeichler61@gmail.com>
2020-10-19 18:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,19 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(kitten) {
-  
+  kittens2=[...kittens,kitten]
+  return kittens2
+}
+
+function prependKitten(kitten) {
+  kittens3=[kitten,...kittens]
+  return kittens3
+}
+
+function removeLastKitten() {
+  return kittens.slice(-2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
 }

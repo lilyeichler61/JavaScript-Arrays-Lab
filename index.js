4183b9266454e95793677b74da669641e79f7674
Lily Eichler <lilyeichler61@gmail.com>
2020-10-19 18:15:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield']
 
-// Add your functions and code here
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(kitten) {
+  
+}
